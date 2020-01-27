c8e327af8d30f7fb47be92e5c1dd7d2adaecef71
Ali Masarwe <alis.masarwe@gmail.com>
2020-01-27 08:21:33 -0800

    Implemented arithemitic operations

--- price.h
+++ price.h
@@ -6,28 +6,57 @@
 #define FIXEDPOINTPRICE_PRICE_H
 
 template <class D, class C>
-class Price{
-public:
-    explicit Price(D dollars = 0, C cents = 0);
-    D getDollars() const { return m_dollars; }
-    C getCents() const { return m_cents; }
-
-private:
-    D m_dollars;
-    C m_cents;
-};
+class Price;
 
 template <class D, class C>
-Price<D,C> whaterver()
+Price<D,C>& operator+(const Price<D,C>& p1, const Price<D,C>& p2)
 {
-    return Price<D,C>;
+    return Price<D,C>(p1.getDollars()+ p2.getDollars(),
+            p1.getCents() + p2.getCents());
+}
+
+template <class D, class C>
+Price<D,C>& operator-(const Price<D,C>& p1, const Price<D,C>& p2)
+{
+    return Price<D,C>(p1.getDollars()- p2.getDollars(),
+                      p1.getCents() - p2.getCents());
+}
+
+template <class D, class C>
+Price<D,C>& operator*(const Price<D,C>& p1, const Price<D,C>& p2)
+{
+    D d_p1 = p1.getDollars(), d_p2 = p2.getDollars();
+    C c_p1 = p1.getCents(), c_p2 = p2.getCents();
+
+    return Price<D,C>(p1.getDollars() * p2.getDollars(),
+                      p1.getCents() * p2.getCents());
+}
+
+template <class D, class C>
+Price<D,C>& operator/(const Price<D,C>& p1, const Price<D,C>& p2)
+{
+    D d_p1 = p1.getDollars(), d_p2 = p2.getDollars();
+    C c_p1 = p1.getCents(), c_p2 = p2.getCents();
+
+    return Price<D,C>(p1.getDollars() / p2.getDollars(),
+                      p1.getCents() / p2.getCents());
+}
+
+template <class D, class C>
+Price<D,C>& operator%(const Price<D,C>& p1, const Price<D,C>& p2)
+{
+    D d_p1 = p1.getDollars(), d_p2 = p2.getDollars();
+    C c_p1 = p1.getCents(), c_p2 = p2.getCents();
+
+    return Price<D,C>(p1.getDollars() % p2.getDollars(),
+                      p1.getCents() % p2.getCents());
 }
 
 template <class D, class C>
 bool operator<(const Price<D,C>& p1, const Price<D,C>& p2)
 {
     return p1.getDollars() < p2.getDollars() ||
-            p1.getDollars() < p2.getDollars();
+           p1.getDollars() < p2.getDollars();
 }
 
 template <class D, class C>
@@ -59,5 +88,20 @@ bool operator>=(const Price<D,C>& p1, const Price<D,C>& p2)
 {
     return !(p1 < p2);
 }
+
+
+template <class D, class C>
+class Price{
+public:
+    explicit Price(D dollars = 0, C cents = 0);
+    D getDollars() const { return m_dollars; }
+    C getCents() const { return m_cents; }
+    static C max_cents = 100;
+
+private:
+    D m_dollars;
+    C m_cents;
+};
+
 
 #endif //FIXEDPOINTPRICE_PRICE_H
