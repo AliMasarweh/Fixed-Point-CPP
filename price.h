c34b8b4ae5f031a997eeb7c241ca909c3d3eb4e4
Ali Masarwe <alis.masarwe@gmail.com>
2020-01-27 07:01:54 -0800

    Implemented relational operators

--- price.h
+++ price.h
@@ -4,13 +4,60 @@
 
 #ifndef FIXEDPOINTPRICE_PRICE_H
 #define FIXEDPOINTPRICE_PRICE_H
+
 template <class D, class C>
 class Price{
 public:
     explicit Price(D dollars = 0, C cents = 0);
+    D getDollars() const { return m_dollars; }
+    C getCents() const { return m_cents; }
+
 private:
     D m_dollars;
     C m_cents;
 };
+
+template <class D, class C>
+Price<D,C> whaterver()
+{
+    return Price<D,C>;
+}
+
+template <class D, class C>
+bool operator<(const Price<D,C>& p1, const Price<D,C>& p2)
+{
+    return p1.getDollars() < p2.getDollars() ||
+            p1.getDollars() < p2.getDollars();
+}
+
+template <class D, class C>
+bool operator>(const Price<D,C>& p1, const Price<D,C>& p2)
+{
+    return p2 < p1;
+}
+
+template <class D, class C>
+bool operator!=(const Price<D,C>& p1, const Price<D,C>& p2)
+{
+    return p1 < p2 || p2 < p1;
+}
+
+template <class D, class C>
+bool operator==(const Price<D,C>& p1, const Price<D,C>& p2)
+{
+    return !(p1 != p2);
+}
+
+template <class D, class C>
+bool operator<=(const Price<D,C>& p1, const Price<D,C>& p2)
+{
+    return !(p1 > p2);
+}
+
+template <class D, class C>
+bool operator>=(const Price<D,C>& p1, const Price<D,C>& p2)
+{
+    return !(p1 < p2);
+}
 
 #endif //FIXEDPOINTPRICE_PRICE_H
