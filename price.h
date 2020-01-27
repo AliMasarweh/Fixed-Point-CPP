a45a9ed5a95f2a5f472033d0af6d6d8fc8ee25e5
Ali Masarwe <alis.masarwe@gmail.com>
2020-01-27 06:52:40 -0800

    Declared the class template and contructor

--- price.h
+++ price.h
@@ -4,5 +4,13 @@
 
 #ifndef FIXEDPOINTPRICE_PRICE_H
 #define FIXEDPOINTPRICE_PRICE_H
+template <class D, class C>
+class Price{
+public:
+    explicit Price(D dollars = 0, C cents = 0);
+private:
+    D m_dollars;
+    C m_cents;
+};
 
 #endif //FIXEDPOINTPRICE_PRICE_H
